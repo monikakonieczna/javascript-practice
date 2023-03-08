64a7b5b8a0ac86601ebee6a2729ec1c53e740e49
Monika Konieczna <konieczna.monika01@gmail.com>
2023-03-08 14:04:23 +0100

    Added implementation for exercise: Word Frequency

--- interview_exercises/13_wordFrequency.js
+++ interview_exercises/13_wordFrequency.js
@@ -2,6 +2,32 @@
  * Given a text string and an integer n, return the n most common words in the file (and the number of their occurrences) in decreasing frequency.
  */
 
-function wordFrequency(txt, n){
-    var words = txt.split(/\s+/);
+function wordFrequency(txt, n) {
+  //convert input text to array of words
+  const words =
+    txt == ""
+      ? []
+      : txt
+          .trim()
+          .split(/\s+/)
+          .map((word) => word.toLowerCase());
+
+  //count unique words
+  const wordCounts = {};
+
+  words.forEach((word) => {
+    if (!wordCounts[word]) {
+      wordCounts[word] = [word, 0];
+    }
+    //Increment hashmap count
+    wordCounts[word][1]++;
+  });
+
+  return Object.values(wordCounts)
+    .sort((a, b) => (b[1] != a[1] ? b[1] - a[1] : b[0] > a[0] ? -1 : 1))
+    .slice(0, n);
 }
+
+const txt = "monika cat string monika cat cat cat cat dog dog ";
+
+console.log(wordFrequency(txt, 2));
